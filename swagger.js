ba200ca9472fe4cf4c9c7df3315925c660eebd77
John T <jonathant4242@gmail.com>
2025-02-01 21:44:19 -0800

    fixed swagger base url

--- swagger.js
+++ swagger.js
@@ -9,14 +9,18 @@ const doc = {
             email: "jonathan.trok@outlook.com"
         }
     },
+    // API server
+    host: "localhost:8080", 
+    basePath: "/", 
+    schemes: ['http'], 
     servers: [
-        {
-            url: "https://cse-341-jt.onrender.com",
-            description: "Production Server (Render Deployment)"
-        },
         {
             url: "http://localhost:8080",
             description: "Local Development Server"
+        },
+        {
+            url: "https://cse-341-jt.onrender.com",
+            description: "Production Server (Render Deployment)"
         }
     ],
     tags: [
@@ -24,16 +28,12 @@ const doc = {
             name: "Contacts",
             description: "Endpoints related to contacts"
         }
-    ],
-    schemes: ['https', 'http']
+    ]
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./server.js', './routes/contacts.js']; // API endpoint files
+const endpointsFiles = ['./server.js', './routes/contacts.js']; // Include API routes
 
-// Generate Swagger JSON
-// Start server AFTER generating Swagger
-// This is a workaround to ensure that the Swagger JSON is generated before the server starts
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-    require('./server.js'); // Start server AFTER generating Swagger
+    require('./server.js'); // Start the server AFTER generating Swagger
 });
